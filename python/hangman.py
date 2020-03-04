5bc401bf76c11ce0c0ed5288687099e949be2a02
Edward Rees <erees.hk@gmail.com>
2020-03-04 00:13:22 -0800

    update of hangman

--- python/hangman.py
+++ python/hangman.py
@@ -32,39 +32,47 @@ def toHiddenWord(word):
     return hiddenChars
 
 
+def compareStrings(actual, hidden):
+  return actual == hidden
+
 def check(char, actualArray):
-    for i in range(len(actualArray)):
-        if(actualArray[i] == char):
-            return i
-    return -1
+    return char in actualArray 
 
 
 def addFailed(char, failed):
     failed.append(char)
 
-
 def setChar(char, actual, hidden, failed):
-    index = check(char, actual)
-    if(index == -1):
-        addFailed(char, failed)
-        return
-    hidden[index] = char
+    if(not check(char, actual)):
+      addFailed(char, failed)
+    else:
+      for (i, chr) in enumerate(actual):
+        if(chr == char):
+          hidden[i] = char
 
+def toString(charArray):
+    return "".join(charArray)
 
 def main():
+    tries = 0
     OPPORTUNITIES = 10
+    word = getRandomWord(parseWords)
     failed = []
-    words = parseWords()
-    print("Number of available words:", len(words))
-    # word = getRandomWord(words)
-    word = "Hello"
     actual = toCharArray(word)
     hidden = toHiddenWord(word)
-    # while(len(failed) < OPPORTUNITIES):
-    setChar("a", actual, hidden, failed)
-    print("Actual:", actual)
-    print("Hidden:", hidden)
-    print("Failed:", failed)
-
-
+    while(len(set(failed)) < OPPORTUNITIES):
+        char = input("Please input a character to try: ")
+        setChar(char, actual, hidden, failed)
+        tries += 1
+        print("Word: ", hidden)
+        if(compareStrings(actual, hidden)):
+          print("You got it in", tries)
+          print("You missed", len(failed), "letters!")
+          break
+    print("Your word was:", toString(actual))
+    if(len(set(failed)) >= OPPORTUNITIES):
+      print("You didn't get your word in", OPPORTUNITIES, "tries!")
+    
 main()
+
+
